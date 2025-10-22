2cf17bffc8748b8a3de297e289353bf2029f9a53
hiro2381 <gamusharanawen@gmail.com>
2025-10-22 10:14:17 +0900

    aiueo

--- 01/main.py
+++ 01/main.py
@@ -1,19 +1,8 @@
-# import utils.function as function
-
-# if __name__ == "__main__":
-#     n = function.add(1, 2)
-#     print(n)
-
-# from utils.function import add
-
-# if __name__ == "__main__":
-#     n = add(1, 2)
-#     print(n)
-
 from utils.count import count_word
 
 if __name__ == "__main__":
     s = "hello world"
-    print("e:", count_word(s , "e"))
-    print("o:", count_word(s , "o"))
-    print("l:", count_word(s , "l"))
+    print("e:", count_word(s , "e")) #1
+    print("o:", count_word(s , "o")) #2
+    print("l:", count_word(s , "l")) #3
+    count_word(s, "aaa")
--- 01/utils/count.py
+++ 01/utils/count.py
@@ -1,3 +1,11 @@
+from utils.function import add
 
 def count_word(s: str, char: str) -> int:
-    return s.count(char)
+    assert isinstance(s, str)
+    assert isinstance(char, str)
+    assert len(char) == 1
+    count = 0
+    for ch in s:
+        if ch == char:
+            count = add(count, 1)
+    return count
