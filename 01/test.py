b8ea2a8668f67ef1bd5b356ea670aabc83a6f862
hiro2381 <gamusharanawen@gmail.com>
2025-10-22 09:27:21 +0900

    aiueo

--- 01/test.py
+++ 01/test.py
@@ -1,3 +1,14 @@
 
 # テスト用
-print("hello")
+def add(x, y):
+    ret = x + y
+    return ret
+
+
+if __name__ == "__main__": 
+    n = add(1, 2)
+    print(n)
+    m = add(3, 4)  
+    print(m)
+    p = add(n, m)
+    print(p)
