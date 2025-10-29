96a4bfd0b837dd07e5b230c590a1d721050116cf
hiro2381 <gamusharanawen@gmail.com>
2025-10-29 09:12:05 +0900

    kadai1

---
+++ 02/class/utils/bank_acconut.py
@@ -0,0 +1,26 @@
+
+
+class BankAccount:
+
+    def __init__(self, name):
+        self.name = name
+        self.balance = 0
+        self.interest_rate = 0.01
+
+    def deposit(self, amount):
+        self.balance += amount
+
+    def withdraw(self, amount):
+        if amount > self.balance:
+            print("Insufficient funds")
+        else:
+            self.balance -= amount
+
+    def get_balance(self):
+        return self.balance
+    
+    def set_interest_rate(self, rate):
+        self.interest_rate = rate
+    
+    def apply_interest(self):
+        self.balance += self.balance * self.interest_rate
