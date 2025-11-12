cf7829e3853da2b5955a2e414d8d7daf4abf22df
hiro2381 <152131994+hiro2381@users.noreply.github.com>
2025-11-12 09:31:49 +0900

    画像ファイルの追加

---
+++ 04/exercise/exercise1.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+
+if __name__ == "__main__":
+    data_dir = "04/exercise/data"
+    data_dir = Path(data_dir).resolve()
+    print("===== problem1 =====")
+    print("===== absolute path =====")
+    print(data_dir)
+
+    print("===== problem2 =====")
+    print("===== all under the data_dir =====")
+    file_list = list(data_dir.glob("*"))
+    for path in enumerate(file_list):
+        print(path[1])
+
+    print("===== problem3 =====")
+    file2_list = list(data_dir.glob("**/*.png"))
+    print(len(file2_list))
